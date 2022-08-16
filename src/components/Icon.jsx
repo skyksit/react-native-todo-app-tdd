0550fcb3ec6a47998eed905b8a906a0949470c27
Richard(Kim,ByoungChul) <skyksit@gmail.com>
2022-08-16 09:47:00 +0900

    chore:delete unused files

--- src/components/Icon.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import FeatIcon from 'react-native-vector-icons/Feather';
-import {IconSizes} from '../utils/constants';
-
-FeatIcon.loadFont();
-
-export const FeatherIcon = ({name, size, color}) => {
-  return <FeatIcon name={name} size={IconSizes[size]} color={color} />;
-};
